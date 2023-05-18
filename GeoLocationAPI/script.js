b7d48bb8fa1455403fa6cf929c3a8bfd0fbb8c20
Andrew Pauls <ajpauls@cloud.edu.pe.ca>
2023-05-18 13:44:12 -0300

    Part1 - Task 9 customize the leaflet tile layer

--- GeoLocationAPI/script.js
+++ GeoLocationAPI/script.js
@@ -3,13 +3,13 @@ navigator.geolocation.getCurrentPosition(
     // console.log(position);
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    // console.log(latitude, longitude);
+    console.log(latitude, longitude);
     // console.log(`https://www.google.com/maps/@${latitude},${longitude},15z`);
     const coords = [latitude, longitude];
 
     var map = L.map("map").setView(coords, 13);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
